a984be7a1d2a81fcdd66853ff9469fe41d1f7f79
alex <alex@slab.org>
2025-06-08 20:01:59 +0100

    remove tidal-vis from sidebar

--- sidebars.js
+++ sidebars.js
@@ -45,7 +45,6 @@ module.exports = {
           "configuration/boot-tidal",
         ],
         "Tidal Tools": [
-          "configuration/tidal-vis",
           "configuration/tidal-listener",
         ],
 
